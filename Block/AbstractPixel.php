b3fbd8d8c61ab7baddc146816616cf8c9699a620
ivanhrytsai <155642341+ivanhrytsaim@users.noreply.github.com>
2026-09-23 15:24:51 +0300

    Fix declaration line in AbstractPixel.php

--- Block/AbstractPixel.php
+++ Block/AbstractPixel.php
@@ -4,7 +4,7 @@
  * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
  */
 
-declare(strict_types=1);
+declare(strict_types=1);test-1
 
 namespace Magefan\FacebookPixel\Block;
 
